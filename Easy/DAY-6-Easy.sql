a9c0d112483cf3b1af908e9330576fa7b48f5f4e
Varalakshmi0814 <Vkotamsetty99@gmail.com>
2026-08-18 23:17:38 +0100

    gender groups with > 5 students and average age < 25

--- Easy/DAY-6-Easy.sql
+++ Easy/DAY-6-Easy.sql
@@ -8,3 +8,24 @@ FROM
     student_master
 WHERE
     student_id % 2 = 0;
+    
+-- ------------------------------------------------------------------------------------------------------------------
+-- 27. Write a query to show gender groups where there are more than 5 students and their average age is less than 25.
+-- ------------------------------------------------------------------------------------------------------------------
+
+SELECT 
+    gender, COUNT(1) AS gender_count
+FROM
+    student_master
+GROUP BY gender
+HAVING COUNT(1) > 4 
+UNION 
+SELECT 
+    gender, ROUND(AVG(age), 0) AS avg_age
+FROM
+    (SELECT 
+        gender, TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) AS age
+    FROM
+        student_master) student_avg_age
+GROUP BY gender
+HAVING AVG(age) > 25;
