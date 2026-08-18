c5c7cfe84ef19f83a2e6a60dea8f0c2fc7f2a2d0
Varalakshmi0814 <Vkotamsetty99@gmail.com>
2026-08-18 23:22:09 +0100

    all students inactive or enrolled before 2020

--- Easy/DAY-7-Easy.sql
+++ Easy/DAY-7-Easy.sql
@@ -8,3 +8,15 @@ FROM
     student_master
 WHERE
     enrollment_date = '2023-01-01';
+
+-- ---------------------------------------------------------------------------------------
+-- 32. Write a query to list all students who are either inactive or enrolled before 2020.
+-- ---------------------------------------------------------------------------------------
+
+SELECT 
+    CONCAT(first_name, ' ', last_name) AS Student_Name
+FROM
+    student_master
+WHERE
+    YEAR(enrollment_date) < '2020-01-01'
+        OR is_active IS FALSE;
