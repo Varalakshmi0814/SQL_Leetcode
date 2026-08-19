05a6e32a02daedc34522939a788b5a35dcadfeb4
Varalakshmi0814 <Vkotamsetty99@gmail.com>
2026-08-19 22:15:05 +0100

    contact number is exactly 10 digits long

--- Easy/DAY-9-Easy.sql
+++ Easy/DAY-9-Easy.sql
@@ -15,3 +15,14 @@ FROM
     student_master
 WHERE
     WEEKDAY(enrollment_date) = 0;
+    
+-- ---------------------------------------------------------------------------------
+-- 42. Write a query to get students whose contact number is exactly 10 digits long.
+-- ---------------------------------------------------------------------------------
+
+SELECT 
+    CONCAT(first_name, ' ', last_name) AS full_name
+FROM
+    student_master
+WHERE
+    LENGTH(contact_number) = 10;
