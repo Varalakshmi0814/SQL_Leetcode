3eeb6b4523b6dd4d87f6dca27572666ab174a99f
Varalakshmi0814 <Vkotamsetty99@gmail.com>
2026-08-20 23:36:35 +0100

    same month

--- Easy/DAY-10-Easy.sql
+++ Easy/DAY-10-Easy.sql
@@ -9,3 +9,15 @@ FROM
     student_master
 GROUP BY YEAR(date_of_birth)
 HAVING COUNT(YEAR(date_of_birth)) >= 3;
+
+-- --------------------------------------------------------------------------------
+-- 47. Write a query to get students who enrolled in the same month they were born.
+-- --------------------------------------------------------------------------------
+
+SELECT 
+    CONCAT(first_name, ' ', last_name) AS full_name
+FROM
+    student_master
+WHERE
+    MONTH(date_of_birth) = MONTH(enrollment_date);
+    
